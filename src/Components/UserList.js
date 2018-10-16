c22b2624363039fed8eef338049504d188cfd80b
Ernesto Jiménez Villaseñor <ernesto@bedu.org>
2018-10-15 23:47:47 -0500

    Added user list styles

--- src/Components/UserList.js
+++ src/Components/UserList.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import UserItem from './UserItem';
 
+import styles from './UserList.module.scss';
+
 
 const UserList = (props) => {
+    console.log(styles);
     return (
-        <ul>
+        <ul className={styles['user-list']}>
             {props.userList.map(
                 user => <UserItem key={user.id} firstName={user.firstName} lastName={user.lastName} userId={user.id} />)}
         </ul>
